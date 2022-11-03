cea95786829c377eeddf994f0347eac41ca221c9
Asraful Alam Azad <64921498+asrafulalamazad@users.noreply.github.com>
2022-11-03 13:38:23 +0600

    simple change

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 let i;
 for ( i= 0; i<=100; i++){
-    console.log (i+' '+"Allah is great");
+    console.log (i+' '+"Allah is great");.
 
 }
 
+for ( i= 0; i<=10; i++){
+    console.log (i+' '+"ASRAFUL ALAM");.
+
+}
 // console.log ("Hello World");
