c7efee789788e7ee59ceeaed0454d156b796004f
asrafulalamazad <asrafulalamazad@gmail.com>
2022-11-03 20:53:45 +0600

    function play

---
+++ fun.js
@@ -0,0 +1,18 @@
+function add(...numbers){
+let sum=0;
+let i;
+    // for(i=0;i<=0;i++){
+    for(i of numbers){
+
+        sum +=i;
+
+
+
+}
+
+console.log (sum) ;
+
+}
+
+add(43,87,98,09,99)
+
---
+++ fun2.js
@@ -0,0 +1,15 @@
+var addFun= function add(...nubmers){
+
+    let sum=0;
+    let i=0;
+    for(i of nubmers){
+        sum +=i;
+
+    }
+
+return sum;
+
+
+}
+console.log(addFun(2,2,233,2,2,2,2,2,2,2));
+// console.log(addFun);
