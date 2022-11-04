135d4de38bc194f1e56eb781bc3e3bc2ed8bc43f
asrafulalamazad <asrafulalamazad@gmail.com>
2022-11-04 23:44:12 +0600

    ArroFun1

---
+++ arrowFunction.js
@@ -0,0 +1,13 @@
+var myArro = (a,b)=>{
+
+// console.log('my First Arrow Funtion')
+console.log(a+b);
+//return a+b;
+
+}
+
+
+//myArro('my First Arrow Funtion user param');
+//myArro(10,90);
+// console.log(myArro(10,90));
+myArro(1,212);
