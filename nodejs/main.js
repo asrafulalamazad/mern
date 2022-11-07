35662b244075703b185c996b0b741cc77fbe184f
asrafulalamazad <asrafulalamazad@gmail.com>
2022-11-08 00:26:49 +0600

    home/about/contact using requst and respons menthod

--- nodejs/main.js
+++ nodejs/main.js
@@ -1,8 +1,22 @@
 var http = require('http');
 
 var server = http.createServer(function(req, res) {
-    res.end("Hello World")
-});
+    if (req.url == '/') {
+        res.writeHead(200, { 'Content-Type': 'Text/html' })
+        res.write('<h1>This Home Page </h1>')
+        res.end();
+    } else if (req.url == '/about') {
+        res.writeHead(200, { 'Content-Type': 'Text/html' })
+        res.write('<h1>This about Page </h1>')
+        res.end();
+    } else if (req.url == '/contact') {
+        res.writeHead(200, { 'Content-Type': 'Text/html' })
+        res.write('<h1>This contact Page </h1>')
+        res.end();
+    }
+
+})
+
 
 server.listen(5050);
 console.log("Server run Successfully")
