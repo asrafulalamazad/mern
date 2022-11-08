78dc96c39604673a26f14562aa24b05ddd025a2b
asrafulalamazad <asrafulalamazad@gmail.com>
2022-11-08 23:38:09 +0600

    Sync fs Module

--- fsModule/index.js
+++ fsModule/index.js
@@ -1,22 +1,30 @@
 var fs = require('fs');
 var http = require('http');
 
+
+//Sync fs module
 var server = http.createServer(function(req, res) {
     if (req.url = '/') {
-        fs.readFile('home.html', function(error, data) {
-            res.writeHead(200, { 'Content-Type': 'text/html' })
-            res.write(data);
-            res.end();
-
-
-        })
-
+        var data = fs.readFileSync('home.html')
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(data);
+        res.end();
 
     }
-
 })
 
+//Async fs system
+// var server = http.createServer(function(req, res) {
+//     if (req.url = '/') {
+//         fs.readFile('home.html', function(error, data) {
+//             res.writeHead(200, { 'Content-Type': 'text/html' })
+//             res.write(data);
+//             res.end();
 
+
+//         })
+//     }
+// })
 
 
 
