5216abfc22b9561770153d216e5c250fa5165bc3
asrafulalamazad <asrafulalamazad@gmail.com>
2022-11-16 00:50:25 +0600

    DeleteOne Success

---
+++ node_with_mongo/insertOne.js
@@ -0,0 +1,34 @@
+var mongodb= require("mongodb");
+var MongoClient= mongodb.MongoClient;
+
+var AtlasURL= "mongodb+srv://asrafuldoc:Azad20121987@cluster0.kxn7sfi.mongodb.net/school?retryWrites=true&w=majority";
+
+
+MongoClient.connect(AtlasURL,function(error,MyMongoClient){
+if (error){
+    console.log("Database connection Failed")
+}
+else{
+    console.log("Database Connection Success")
+    InsertData(MyMongoClient);
+    
+}
+});
+
+function InsertData(MyMongoClient){
+    var myDb= MyMongoClient.db('school');
+    var myCollection= myDb.collection('students');
+
+    var myData= {name:"Ahnaf Bin Asraf", phone: "01670298486", class: "One"};
+
+  //  myCollection.insertOne(myData);
+
+    myCollection.insertOne(myData,function(error){
+        if(error) {
+            console.log('Inserted Failed')
+        } else
+        {
+            console.log('Inserted');
+        }
+    })
+}
