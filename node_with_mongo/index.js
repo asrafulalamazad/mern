af77bc32a236e76ef8a35c3401f290c65cae135e
asrafulalamazad <asrafulalamazad@gmail.com>
2022-11-15 23:15:11 +0600

    mongo db connection ok with db select (1st time)

---
+++ node_with_mongo/index.js
@@ -0,0 +1,14 @@
+var mongodb= require("mongodb");
+var MongoClient= mongodb.MongoClient;
+
+var AtlasURL= "mongodb+srv://asrafuldoc:Azad20121987@cluster0.kxn7sfi.mongodb.net/?retryWrites=true&w=majority";
+
+
+MongoClient.connect(AtlasURL,function(error){
+if (error){
+    console.log("Database connection Failed")
+}
+else{
+    console.log("Database Connectd")
+}
+})
