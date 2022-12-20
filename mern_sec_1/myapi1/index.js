51c2c739b29c555fb1a8da25d3799605e385d6d3
asrafulalamazad <asrafulalamazad@gmail.com>
2022-12-20 23:48:55 +0600

    update dotEnv lib using variable

--- mern_sec_1/myapi1/index.js
+++ mern_sec_1/myapi1/index.js
@@ -1,10 +1,13 @@
 const app= require("./app.js")
 const dotEnv= require('dotenv');
 
-dotEnv.config({path: "./config.env"});
 
-app.listen(process.env.RUNNING_PORT, function(){
-    console.log('server port running. using dotEnv success')
+//available path 
+dotEnv.config({path: "./config.env"});
+const portNo= process.env.RUNNING_PORT;
+app.listen(portNo, function(){
+  //  console.log('server port running. using dotEnv success')
+    console.log("Running Port = "+portNo)
 })
 
 // app.listen(8080, function(){
