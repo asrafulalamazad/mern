e7296f0f93590f9d1d773e5132ab491373dc480d
asrafulalamazad <asrafulalamazad@gmail.com>
2022-12-20 23:39:46 +0600

    using dotEnv lib

--- mern_sec_1/myapi1/index.js
+++ mern_sec_1/myapi1/index.js
@@ -1,5 +1,12 @@
 const app= require("./app.js")
+const dotEnv= require('dotenv');
 
-app.listen(8080, function(){
-    console.log("sucess")
+dotEnv.config({path: "./config.env"});
+
+app.listen(process.env.RUNNING_PORT, function(){
+    console.log('server port running. using dotEnv success')
 })
+
+// app.listen(8080, function(){
+//     console.log("sucess")
+// })
