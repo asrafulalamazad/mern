0d93a5b87937e1c1b6eaa0f6ce1c2c3b1438d29a
asrafulalamazad <asrafulalamazad@gmail.com>
2023-03-01 23:07:11 +0600

    react start

---
+++ react_practice/myapp/my-app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+          <h1>Hello</h1>
+          <h1>Ahnaf</h1>
+        </div>
+    );
+};
+
+export default App;
