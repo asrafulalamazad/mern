600656a88a8ba25959ef5300041f2b3b57684ade
asrafulalamazad <asrafulalamazad@gmail.com>
2023-03-05 00:11:40 +0600

    update

--- react_practice/firstapp/my-app/src/components/HomeBanner.js
+++ react_practice/firstapp/my-app/src/components/HomeBanner.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const HomeBanner = (props) => {
+    const [color, setColor]=useState("white")
+    const newColor = ()=>{
+        setColor("Black")
+    }
+
+
     return (
         <div>
-            <h1>Welcome</h1>
-            <h2>{props.title}</h2>
-            <h3>{props.subtitle}</h3>
-            <h3>{props.info.name}</h3>
-            <h3>{props.info.city}</h3>
+            <h1>{color}</h1>
+            <button onClick= {newColor}>Change</button>
         </div>
     );
 };
