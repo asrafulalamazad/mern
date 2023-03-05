45aa4fa805d0c9fd43a56842d20b296b65dbdb74
asrafulalamazad <asrafulalamazad@gmail.com>
2023-03-06 00:40:42 +0600

    react use Effect

--- react_practice/firstapp/my-app/src/components/HomeBanner.js
+++ react_practice/firstapp/my-app/src/components/HomeBanner.js
@@ -1,27 +1,78 @@
-import React, {useRef} from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from "axios";
 
 const HomeBanner = () => {
-    let userName= useRef();
-    let password= useRef();
 
-    const submitForm=()=> {
-        let user = userName.current.value;
-        let passKey = password.current.value;
-        alert("user: "+user);
-        alert("Password: "+passKey);
-    }
+        const [todo, setTodo]= useState("Please Wait")
+
+
+    useEffect(()=>{
+
+        // api calling
+        axios.get("https://jsonplaceholder.typicode.com/todos/")
+            .then((res)=>{
+                setTimeout(
+                    ()=>{                setTodo(res.data)
+                    },3000
+                )
+
+                // alert(res.status);
+                // alert(res.data);
+            })
+            .catch((error)=>{
+            })
+
+        //alert("Welcome")
+
+    })
 
 
     return (
         <div>
-            <input ref={userName} placeholder="User Name" type="text"/>
-            <input ref={password} placeholder="Password" type="password"/>
-            <button onClick={submitForm}>Get Data</button>
+            {JSON.stringify(todo)}
         </div>
     );
 };
 
 export default HomeBanner;
+
+
+
+
+
+
+
+
+
+
+
+
+// import React, {useRef} from 'react';
+//
+// const HomeBanner = () => {
+//     let userName= useRef();
+//     let password= useRef();
+//
+//     const submitForm=()=> {
+//         let user = userName.current.value;
+//         let passKey = password.current.value;
+//         alert("user: "+user);
+//         alert("Password: "+passKey);
+//     }
+//
+//
+//     return (
+//         <div>
+//             <input ref={userName} placeholder="User Name" type="text"/>
+//             <input ref={password} placeholder="Password" type="password"/>
+//             <button onClick={submitForm}>Get Data</button>
+//         </div>
+//     );
+// };
+//
+// export default HomeBanner;
+
+
 
 
 
