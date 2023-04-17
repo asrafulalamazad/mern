13543d2e56244ac1154e0752fd8f582f23cebc91
asrafulalamazad <asrafulalamazad@gmail.com>
2023-04-18 01:37:03 +0600

    combine structure

---
+++ MERN_Stack/combaine_stack_app/client/src/App.js
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p className="display-1">Front End </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
